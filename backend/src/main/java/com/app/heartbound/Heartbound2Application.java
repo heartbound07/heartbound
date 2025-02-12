612ba2553f01b36d7474d7d5c0c57f5944e963a3
Julian Noronha <42124467+juliannoronha@users.noreply.github.com>
2025-02-12 18:02:20 -0500

    Initial Commit

---
+++ backend/src/main/java/com/app/heartbound/Heartbound2Application.java
@@ -0,0 +1,13 @@
+package com.app.heartbound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Heartbound2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Heartbound2Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/app/heartbound/Heartbound2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.heartbound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Heartbound2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
