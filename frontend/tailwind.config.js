a82008f03a4cd808e25448b2eeff6e15b34868ce
Julian Noronha <42124467+juliannoronha@users.noreply.github.com>
2025-02-12 23:00:50 -0500

    Added CSS to login page

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          900: '#111827',
+          800: '#1F2937',
+          700: '#374151',
+          600: '#4B5563',
+        }
+      }
+    },
+  },
+  plugins: [],
+} 
