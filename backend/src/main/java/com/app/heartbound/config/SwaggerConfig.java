a4bbc0527e6bcac3c541743c20b2af3c6a1267f5
Julian Noronha <42124467+juliannoronha@users.noreply.github.com>
2025-02-15 21:39:01 -0500

    add JWT Bearer security scheme to SwaggerConfig

--- backend/src/main/java/com/app/heartbound/config/SwaggerConfig.java
+++ backend/src/main/java/com/app/heartbound/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.app.heartbound.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +18,13 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("HeartBound API")
                         .version("1.0")
-                        .description("API documentation for the HeartBound application."));
+                        .description("API documentation for the HeartBound application."))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 
     @Bean
