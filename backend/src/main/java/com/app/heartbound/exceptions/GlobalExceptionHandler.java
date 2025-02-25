96990274f36f637ccb2e4eeea3cb78d49c5d8a91
Julian Noronha <42124467+juliannoronha@users.noreply.github.com>
2025-02-25 17:33:43 -0500

    Added GlobalExceptionHandler

--- backend/src/main/java/com/app/heartbound/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/app/heartbound/exceptions/GlobalExceptionHandler.java
@@ -6,8 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-// If not already defined, these custom exceptions should be created in your project.
 import com.app.heartbound.exceptions.ResourceNotFoundException;
 import com.app.heartbound.exceptions.UnauthorizedOperationException;
 
